e87fdb20690fadfb722155d20499e13f2d9cdd99
Alicja.J.Braniewska <alicja.j.braniewska@gmail.com>
2017-05-22 01:51:22 +0200

    Add next tasks

--- src/random/CheckingParenthesis.java
+++ src/random/CheckingParenthesis.java
@@ -2,39 +2,42 @@ package random;
 
 import java.util.Stack;
 
-// zadanie działa, ale jeszcze nie do końca poprawnie
+// działa poprawnie
 public class CheckingParenthesis {
     boolean isValid(String expression) {
 
-        //char[] myChar = expression.toCharArray();
         Stack<Character> myStack = new Stack<>(); // nie musi być "new Stack<Character>()" bo ten sam typ generyczny
-        System.out.println(myStack + "początek stacku");
 
         for (int i = 0; i < expression.length(); i++) {
-            System.out.println(expression.charAt(i)); // spr czy poprawnie czyta
-            //System.out.print(myChar[i]); spr czy poprawnie czyta
             if ((expression.charAt(i) == '(') || (expression.charAt(i) == '{') || (expression.charAt(i) == '[')) {
                 myStack.push(expression.charAt(i));
-                System.out.println("obecny stack " + myStack);
             } else if ((expression.charAt(i) == ')') || (expression.charAt(i) == '}') || (expression.charAt(i) == ']')) {
-                /* bo MUSZE! jeszcze sprawdzić czy element na gorze stosu to jest ten sam nawias zamykający,
+                /* MUSZE jeszcze sprawdzić czy element na gorze stosu to jest ten sam nawias zamykający,
                 bez poniższego źle liczyło przy np. "([]{(}))"
                  */
-                if (expression.charAt(i) == myStack.peek()) {
+                if (leftParenthesis(expression.charAt(i)) == myStack.peek()) {
                     myStack.pop();
-                    System.out.print("obecny stack " + myStack);
                 }
             }
         }
+        return myStack.isEmpty();
+    }
 
-        if (myStack.isEmpty()) {
-            return true;
-        } else return false;
+    private char leftParenthesis(char rightParenthesis) {
+        if (rightParenthesis == ')') {
+            return '(';
+        } else if (rightParenthesis == ']') {
+            return '[';
+        } else if (rightParenthesis == '}') {
+            return '{';
+        } else {
+            throw new IllegalArgumentException("Argument is not left parenthese");
+        }
     }
 
     public static void main(String[] args) {
         CheckingParenthesis cp = new CheckingParenthesis();
-        boolean valid = cp.isValid("([]{}())");
+        boolean valid = cp.isValid("([]{}(())");
         System.out.println("Is expression valid? " + valid);
     }
 }
